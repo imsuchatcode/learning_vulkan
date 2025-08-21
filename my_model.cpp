c54a9f266ad3b8c5510c104926273f7e44299dbb
imsuchatcode <firekatana1209@gmail.com>
2025-08-21 15:22:43 +0700

    make more function in mymodel.cpp

--- my_model.cpp
+++ my_model.cpp
@@ -1,6 +1,54 @@
 #include "my_model.hpp"
 
+#include <cassert>
+#include <cstring>
+
 namespace my{
-    myModel::myModel(Device &device, const std::vector<Vertex> &vertices) {}
-    myModel::~myModel(){}
+    myModel::myModel(Device &device, const std::vector<Vertex> &vertices) : myDevice{device} {
+        createVertexBuffers(vertices);
+    }
+    myModel::~myModel(){
+        vkDestroyBuffer(myDevice.device(), vertexBuffer, nullptr);
+        vkFreeMemory(myDevice.device(), vertexBufferMemory, nullptr);
+    }
+
+    void myModel::createVertexBuffers(const std::vector<Vertex> &vertices){
+        vertexCount = static_cast<uint32_t>(vertices.size());
+        assert(vertexCount >= 3 && "vertex count must at least be 3");
+        VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
+        myDevice.createBuffer(bufferSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, vertexBuffer, vertexBufferMemory);
+
+        void *data;
+        vkMapMemory(myDevice.device(), vertexBufferMemory, 0, bufferSize, 0, &data);
+        memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+        vkUnmapMemory(myDevice.device(), vertexBufferMemory);
+    }
+
+    void myModel::draw(VkCommandBuffer commandBuffer){
+        vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0); 
+
+    }
+
+    void myModel::bind(VkCommandBuffer commandBuffer){
+        VkBuffer buffers[] = {vertexBuffer};
+        VkDeviceSize offsets[] = {0};
+        
+        vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
+    }
+
+    std::vector<VkVertexInputBindingDescription> myModel::Vertex::getBindingDescriptions(){
+        std::vector<VkVertexInputBindingDescription> bindingDescription(1);
+        bindingDescription[0].binding = 0;
+        bindingDescription[0].inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+        bindingDescription[0].stride = sizeof(Vertex);
+        return bindingDescription;
+    }
+    std::vector<VkVertexInputAttributeDescription> myModel::Vertex::getAttributeDescriptions(){
+        std::vector<VkVertexInputAttributeDescription> attributeDescription(1);
+        attributeDescription[0].binding = 0;
+        attributeDescription[0].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescription[0].location = 0;
+        attributeDescription[0].offset = 0;
+        return attributeDescription;
+    }
 }
