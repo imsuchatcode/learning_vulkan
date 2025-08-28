757ca7f7c0b2a1aaade19515717fb3dfc5e4166e
imsuchatcode <firekatana1209@gmail.com>
2025-08-28 21:28:33 +0700

    temporary movement

--- first_app.cpp
+++ first_app.cpp
@@ -114,6 +114,9 @@ void FirstApp::freeCommandBuffers(){
 }
 
 void FirstApp::recordCommandBuffer(int imageIndex){
+    static int frame = 0;
+    frame = (frame + 1) % 1000;
+
     VkCommandBufferBeginInfo beginInfo{};
     beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
@@ -131,7 +134,7 @@ void FirstApp::recordCommandBuffer(int imageIndex){
     renderPassInfo.renderArea.extent = mySwapChain->getSwapChainExtent();
 
     std::array<VkClearValue, 2> clearValues{};
-    clearValues[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+    clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
     clearValues[1].depthStencil = {1.0f, 0};
 
     renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
@@ -155,7 +158,7 @@ void FirstApp::recordCommandBuffer(int imageIndex){
     
     for (int j = 0; j < 4; j++){
         SimplePushConstantData push{};
-        push.offset = {0.0f, -0.4f + j * 0.25f};
+        push.offset = {-0.5f + frame * 0.0007f, -0.4f + j * 0.25f};
         push.color = {0.0f, 0.0f, 0.2f + j * 0.2f};
 
         vkCmdPushConstants(commandBuffers[imageIndex], pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantData), &push);
