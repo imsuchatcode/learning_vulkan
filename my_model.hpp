47c6bf8c7076fdd377fdb12de2f6da39af23c5c8
imsuchatcode <firekatana1209@gmail.com>
2025-11-04 23:19:55 +0700

    refractor mymodel.hpp to prepare for indexbuffer

--- my_model.hpp
+++ my_model.hpp
@@ -9,32 +9,46 @@
 #include <vector>
 
 namespace my{
-    //copy vertex data from the cpu and transfer it to the gpu
-    class MyModel{
-        public:
-            struct Vertex{
-                glm::vec3 position;
-                glm::vec3 color;
 
-                static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
-                static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
-            };
-            
-            MyModel(Device &device, const std::vector<Vertex> &vertices);
-            ~MyModel();
+//copy vertex data from the cpu and transfer it to the gpu
+class MyModel{
+    public:
+        struct Vertex{
+            glm::vec3 position;
+            glm::vec3 color;
 
-            // must delete copy because it MyModel mangage buffer and memory object
-            MyModel(const MyModel &) = delete;
-            MyModel &operator=(const MyModel &) = delete;
+            static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
+            static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+        };
 
-            void bind(VkCommandBuffer commandBuffer);
-            void draw(VkCommandBuffer commandBuffer);
-        private:
-            void createVertexBuffers(const std::vector<Vertex> &vertices);
+        struct Builder{
+            std::vector<Vertex> vertices{};
+            std::vector<uint32_t> indicies{};
+        };
+        
+        MyModel(Device &device, const MyModel::Builder &builder);
+        ~MyModel();
 
-            Device& myDevice;
-            VkBuffer vertexBuffer;
-            VkDeviceMemory vertexBufferMemory;
-            uint32_t vertexCount;
-    };
+        // must delete copy because it MyModel mangage buffer and memory object
+        MyModel(const MyModel &) = delete;
+        MyModel &operator=(const MyModel &) = delete;
+
+        void bind(VkCommandBuffer commandBuffer);
+        void draw(VkCommandBuffer commandBuffer);
+    private:
+        void createVertexBuffers(const std::vector<Vertex> &vertices);
+        void createIndexBuffers(const std::vector<uint32_t> &indicies);
+
+        Device& myDevice;
+
+        VkBuffer vertexBuffer;
+        VkDeviceMemory vertexBufferMemory;
+        uint32_t vertexCount;
+
+        bool haveIndexBuffer = false;
+        VkBuffer indexBuffer;
+        VkDeviceMemory indexBufferMemory;
+        uint32_t indexCount;
+};
+
 }
