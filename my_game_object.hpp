1123aa188f4c687e5d0372f811ab33421f14701f
imsuchatcode <firekatana1209@gmail.com>
2025-11-12 22:44:37 +0700

    fixed the problem scaling with light glith

--- my_game_object.hpp
+++ my_game_object.hpp
@@ -11,17 +11,8 @@ struct TransformComponent {
     glm::vec3 scale{1.f, 1.f, 1.f};
     glm::vec3 rotation{};
 
-    glm::mat4 mat4(){
-
-        auto transform = glm::translate(glm::mat4{1.f}, translation);
-
-        transform = glm::rotate(transform, rotation.y, {0.0f , 1.0f, 0.0f});
-        transform = glm::rotate(transform, rotation.x, {1.0f , 0.0f, 0.0f});
-        transform = glm::rotate(transform, rotation.z, {0.0f , 0.0f, 1.0f});
-
-        transform = glm::scale(transform, scale);
-        return transform;
-    }
+    glm::mat4 mat4();
+    glm::mat3 normalMatrix();
 };
 
 class MyGameObject{
--- simple_render_system.cpp
+++ simple_render_system.cpp
@@ -13,7 +13,7 @@ namespace my{
 
 struct SimplePushConstantData{
     glm::mat4 transform{1.f};
-    glm::mat4 modelMatrix{1.f};
+    glm::mat4 normalMatrix{1.f};
 };
 
 SimpleRenderSystem::SimpleRenderSystem(Device &device, VkRenderPass renderPass) : myDevice{device}{
@@ -64,7 +64,7 @@ void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::v
         SimplePushConstantData push{};
         auto modelMatrix = obj.transform.mat4();
         push.transform = projectionView * modelMatrix;
-        push.modelMatrix = modelMatrix;
+        push.normalMatrix = obj.transform.normalMatrix();
 
         vkCmdPushConstants(commandBuffer, pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantData), &push);
 
